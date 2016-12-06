599434be906c501acb4616c87e616ff5ab7486d3
Neylson Crepalde <neylsoncrepalde@gmail.com>
2016-12-06 00:27:01 -0200

    Update app.R

--- app.R
+++ app.R
@@ -14,59 +14,59 @@ library(sand)
 
 # Define UI for application that draws a histogram
 ui <- navbarPage("Análise de Redes Sociais - GIARS",
-  
-  tabPanel("Apresentação",
-           sidebarLayout(
-             sidebarPanel(
-               img(src="http://www.giars.ufmg.br/images/logo.png", height=119, width=250)
-             ),
-             mainPanel(
-              h1("Aplicação de Análise de Redes Sociais - GIARS"),
-              
-              p("Esta é uma aplicação de testes de Análise de Redes Sociais desenvolvido 
-                por Neylson Crepalde e pelo GIARS (Grupo Interdisciplinar de Pesquisa em Análise
-                de Redes Sociais). Seu objetivo é facilitar o aprendizado e fomentar o uso 
-                de ferramentas de ARS no Brasil."),
-              br(),
-              p("Esta aplicação está em desenvolvimento!")
-             )
-           )),             
-    
-   # Application title
-   #titlePanel("Análise de Redes Sociais"),
-   
-  tabPanel("Rede de Lazega", 
-  # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-        img(src="http://www.giars.ufmg.br/images/logo.png", height=119, width=250),
-         helpText("Trabalhando com a rede de advogados de E. Lazega."),
-         textInput("text", label = "Defina o título da rede", value = "Advogados (Lazega)"),
-         selectInput("algoritmo", label = "Defina o algoritmo de visualização",
-                     choices = c("Fruchterman-Reingold","Kamada-Kawai",
-                                 "Escalonamento Multidimensional","Circular"),
-                     selected = "Fruchterman-Reingold"),
-         
-         selectInput("metrica", label = "Métrica de Rede",
-                     choices = c("Nenhum","Centralidade de Grau",
-                                 "Centralidade de Intermediação",
-                                 "Centralidade de Proximidade","Constraint"),
-                     selected = "Nenhum"),
-         
-         
-         submitButton(text = "Atualizar")
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("net")
-      )
-   )
-))
+                 
+                 tabPanel("Apresentação",
+                          sidebarLayout(
+                            sidebarPanel(
+                              img(src="http://www.giars.ufmg.br/images/logo.png", height=119, width=250)
+                            ),
+                            mainPanel(
+                              h1("Aplicação de Análise de Redes Sociais - GIARS"),
+                              
+                              p("Esta é uma aplicação de testes de Análise de Redes Sociais desenvolvido 
+                                por Neylson Crepalde e pelo GIARS (Grupo Interdisciplinar de Pesquisa em Análise
+                                de Redes Sociais). Seu objetivo é facilitar o aprendizado e fomentar o uso 
+                                de ferramentas de ARS no Brasil."),
+                              br(),
+                              p("Esta aplicação está em desenvolvimento!")
+                              )
+                          )),             
+                 
+                 # Application title
+                 #titlePanel("Análise de Redes Sociais"),
+                 
+                 tabPanel("Rede de Lazega", 
+                          # Sidebar with a slider input for number of bins 
+                          sidebarLayout(
+                            sidebarPanel(
+                              img(src="http://www.giars.ufmg.br/images/logo.png", height=119, width=250),
+                              helpText("Trabalhando com a rede de advogados de E. Lazega."),
+                              textInput("text", label = "Defina o título da rede", value = "Advogados - Componente Principal"),
+                              selectInput("algoritmo", label = "Defina o algoritmo de visualização",
+                                          choices = c("Fruchterman-Reingold","Kamada-Kawai",
+                                                      "Escalonamento Multidimensional","Circular"),
+                                          selected = "Fruchterman-Reingold"),
+                              
+                              selectInput("metrica", label = "Métrica de Rede",
+                                          choices = c("Nenhum","Centralidade de Grau",
+                                                      "Centralidade de Intermediação",
+                                                      "Centralidade de Proximidade","Constraint"),
+                                          selected = "Nenhum"),
+                              
+                              
+                              submitButton(text = "Atualizar")
+                            ),
+                            
+                            # Show a plot of the generated distribution
+                            mainPanel(
+                              plotOutput("net")
+                            )
+                          )
+                 ))
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-   
+  
   dataInput = reactive({
     lazega = upgrade_graph(lazega)
     lazega = delete_vertices(lazega, c(8,23))
@@ -75,24 +75,24 @@ server <- function(input, output) {
   output$net <- renderPlot({
     
     algo = switch(input$algoritmo,
-                       "Fruchterman-Reingold" = layout_with_fr,
-                       "Kamada-Kawai" = layout_with_kk,
-                       "Escalonamento Multidimensional" = layout_with_mds,
-                       "Circular" = layout_in_circle)
+                  "Fruchterman-Reingold" = layout_with_fr,
+                  "Kamada-Kawai" = layout_with_kk,
+                  "Escalonamento Multidimensional" = layout_with_mds,
+                  "Circular" = layout_in_circle)
     
     escore = switch(input$metrica,
                     "Nenhum" = 15,
                     "Centralidade de Grau" = degree(dataInput()),
-                    "Centralidade de Intermediação" = ( betweenness(dataInput())/2 ),
-                    "Centralidade de Proximidade" = (closeness(dataInput())*100 ),
-                    "Constraint" = constraint(dataInput())
-                    )
+                    "Centralidade de Intermediação" = betweenness(dataInput())/4,
+                    "Centralidade de Proximidade" = closeness(dataInput())*700,
+                    "Constraint" = constraint(dataInput())*20
+    )
     
     
-      plot.igraph(dataInput(), layout=algo, vertex.size=escore, 
-                  #vertex.color='SkyBlue2',
-           main = input$text)
-   })
+    plot.igraph(dataInput(), layout=algo, vertex.size=escore, 
+                #vertex.color='SkyBlue2',
+                main = input$text)
+  })
 }
 
 # Run the application 

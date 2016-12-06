468c6f88f1323e48533988f2c9806ac8af336ec2
Neylson Crepalde <neylsoncrepalde@gmail.com>
2016-12-06 02:20:08 -0200

    Update app.R

--- app.R
+++ app.R
@@ -35,8 +35,9 @@ ui <- navbarPage("Análise de Redes Sociais - GIARS", theme = "slate_bootstrap.c
                  
                  # Application title
                  #titlePanel("Análise de Redes Sociais"),
-                 
-                 tabPanel("Rede de Lazega", 
+                 navbarMenu("Redes - Exemplos",
+                   
+                 tabPanel("Rede de Advogados", 
                           # Sidebar with a slider input for number of bins 
                           sidebarLayout(
                             sidebarPanel(style = "background-color: #BA1723;",
@@ -64,11 +65,44 @@ ui <- navbarPage("Análise de Redes Sociais - GIARS", theme = "slate_bootstrap.c
                               plotOutput("net")
                             )
                           )
-                 ))
+                 ),
+                 
+                 tabPanel("Rede de Blogs", 
+                          # Sidebar with a slider input for number of bins 
+                          sidebarLayout(
+                            sidebarPanel(style = "background-color: #BA1723;",
+                                         
+                                         img(src="http://www.giars.ufmg.br/images/logo.png", height=107, width=225),
+                                         helpText("Trabalhando com a subrede de blogs de política Franceses"),
+                                         textInput("text2", label = "Defina o título da rede", value = "Blogs de Política Franceses"),
+                                         selectInput("algoritmo2", label = "Defina o algoritmo de visualização",
+                                                     choices = c("Fruchterman-Reingold","Kamada-Kawai",
+                                                                 "Escalonamento Multidimensional","Circular"),
+                                                     selected = "Fruchterman-Reingold"),
+                                         
+                                         selectInput("metrica2", label = "Métrica de Rede",
+                                                     choices = c("Nenhum","Centralidade de Grau",
+                                                                 "Centralidade de Intermediação",
+                                                                 "Centralidade de Proximidade","Constraint"),
+                                                     selected = "Nenhum"),
+                                         
+                                         
+                                         submitButton(text = "Atualizar")
+                            ),
+                            
+                            # Show a plot of the generated distribution
+                            mainPanel(
+                              plotOutput("net2")
+                            )
+                          )
+                 )
+          )
+)
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
   
+  # Rede de Advogados Lazega
   dataInput = reactive({
     lazega = upgrade_graph(lazega)
     lazega = delete_vertices(lazega, c(8,23))
@@ -91,10 +125,37 @@ server <- function(input, output) {
     )
     
     
-    plot.igraph(dataInput(), layout=algo, vertex.size=escore, 
+    plot.igraph(dataInput(), layout=algo, vertex.size=escore, vertex.label.cex=.8,
                 #vertex.color='SkyBlue2',
                 main = input$text)
   })
+  
+  # Rede de blogs
+  dataInput2 = reactive({
+    blogs = upgrade_graph(fblog)
+  })
+  
+  output$net2 <- renderPlot({
+    
+    algo2 = switch(input$algoritmo2,
+                  "Fruchterman-Reingold" = layout_with_fr,
+                  "Kamada-Kawai" = layout_with_kk,
+                  "Escalonamento Multidimensional" = layout_with_mds,
+                  "Circular" = layout_in_circle)
+    
+    escore2 = switch(input$metrica2,
+                    "Nenhum" = 5,
+                    "Centralidade de Grau" = degree(dataInput2())/2,
+                    "Centralidade de Intermediação" = betweenness(dataInput2())/100,
+                    "Centralidade de Proximidade" = closeness(dataInput2())*2000,
+                    "Constraint" = constraint(dataInput2())*40
+    )
+    
+    plot.igraph(dataInput2(), layout=algo2, vertex.size=escore2, vertex.label=NA,
+                #vertex.color='SkyBlue2',
+                main = input$text2)
+  })
+  
 }
 
 # Run the application 
